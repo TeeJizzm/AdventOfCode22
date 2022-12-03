bebe3323c368068cec788d6e8d191b98cf6cd791
Teejizzm <teejizzm@gmail.com>
2022-12-03 01:50:23 +0000

    Day 2 - Part 1 complete

--- day02.py
+++ day02.py
@@ -5,28 +5,60 @@ from tools import rockpaperscissors as rps
 
 ############################
 
+points = {
+    "A" : 1,
+    "B" : 2,
+    "C" : 3,
+    "X" : 1,
+    "Y" : 2,
+    "Z" : 3
+    }
+
+rpsMatrix = [
+        [3,0,6], # 6 points for a win,
+        [6,3,0], # 3 points for a tie,
+        [0,6,3]  # 0 points for a loss
+        ]  
+
+############################
+
 def day2(filepath):
     print("Day 2 - Rock, Paper, Scissors")
 
     rounds = tl.toLists(filepath, "\n", " ")
 
-    opponentScore, responseScore = 0, 0
+    playerPoints = sum(points[player] + rpsMatrix[points[player]-1][points[opponent]-1] for opponent, player in rounds)
+    opponentPoints = sum(points[player] + (6 - rpsMatrix[points[player]-1][points[opponent]-1]) for opponent, player in rounds)
 
-    for round in rounds:
+    opponentScore, playerScore, playerScore2 = 0, 0, 0
+
+    roundScore = []
+
+    for opponent,player in rounds:
         
-        resPlay = rps.playPoints(round[1])
-        oppPlay = rps.playPoints(round[0])        
+        roundScore.append(points[player] + rpsMatrix[points[opponent]-1][points[player]-1])
 
-        resScore = rps.scorePoints(oppPlay, resPlay)
-        oppScore = 6 - resScore
 
-        responseScore += (resScore + resPlay)
-        opponentScore += (oppScore + oppPlay)
+        pointsOpp, pointsPla = points[opponent], points[player]
 
-        print(oppPlay)
-        print(resPlay)
+        #print("Played: ", pointsOpp, " ", pointsPla)
 
-    print(responseScore)
+        scorePla = rpsMatrix[pointsOpp-1][pointsPla-1]
+        scoreOpp = 6 - scorePla
+
+        roundPla = pointsPla + scorePla
+        roundOpp = pointsOpp + scoreOpp
+
+        playerScore2 += roundPla
+        opponentScore += roundOpp
+
+    playerScore = sum(roundScore)
+
+    print("Short calc= ", playerScore)
+
+    print("Long Calc = ", playerScore2)
+
+    print("One liner = ", playerPoints)
 
 ############################
 
@@ -36,7 +68,7 @@ if __name__ == "__main__":
     # Change file
     #######
     day = "day02" 
-    file = "example1.txt"
+    file = "input1.txt"
     #######
     
     # Get absolute filepath of file
--- day02/day02.md
+++ day02/day02.md
@@ -20,7 +20,7 @@ Winning every time would be suspicious, so the responses must have been carefull
     Play:       Opponent:       Response:       Points:
     Rock        A = Rock        X = Rock        1
     Paper       B = Paper       Y = Paper       2
-    Scissors    C = Scissors    C = Scissors    3
+    Scissors    C = Scissors    Z = Scissors    3
 
     Scored Points:
     Loss:   0
@@ -38,9 +38,11 @@ Since you can't be sure if the Elf is trying to help you or trick you, you shoul
 
 For example, suppose you were given the following strategy guide:
 
-A Y\
-B X\
+``` ex
+A Y
+B X
 C Z
+```
 
 This strategy guide predicts and recommends the following:
 
@@ -57,6 +59,22 @@ In this example, if you were to follow the strategy guide, you would get a total
 
 ### Answer
 
+`14264`
+
 ## Part 2
 
+The Elf finishes helping with the tent and sneaks back over to you. "Anyway, the second column says how the round needs to end: X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win. Good luck!"
+
+The total score is still calculated in the same way, but now you need to figure out what shape to choose so the round ends as indicated. The example above now goes like this:
+
+- In the first round, your opponent will choose Rock (A), and you need the round to end in a draw (Y), so you also choose Rock. This gives you a score of 1 + 3 = 4.
+- In the second round, your opponent will choose Paper (B), and you choose Rock so you lose (X) with a score of 1 + 0 = 1.
+- In the third round, you will defeat your opponent's Scissors with Rock for a score of 1 + 6 = 7.
+
+Now that you're correctly decrypting the ultra top secret strategy guide, you would get a total score of 12.
+
+**Following the Elf's instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide?**
+
 ### Answer
+
+``

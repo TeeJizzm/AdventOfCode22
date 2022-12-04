8c8847be7a646263fe9387fee06ba0bb6d2eebd5
teej <tylerjay2010@live.co.uk>
2022-12-04 16:18:11 +0000

    Day03 - Part 1 Complete

--- day03/day03.py
+++ day03/day03.py
@@ -1,24 +1,47 @@
 import os
 
-#######
-day = "day03"
-file = "example.txt"
-#######
+# Index + 1 for priority of a character
+priority = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-# Get absolute filepath of file
-cwd = os.getcwd()
-filepath = os.path.join(cwd, day, file)
-
-with open(filepath) as file:
-
-    rucksacks = file.read().splitlines()
-
+def day03(filepath):
+    # Get list of rucksack contents
+    with open(filepath) as file:
+        rucksacks = file.read().splitlines()
+        
+    items_in_both = []
+    
+    # For each rucksack
     for rucksack in rucksacks:
         
-        print(len(rucksack))
-
-        half1 = rucksack[0:len(rucksack)//2]
-        half2 = rucksack[len(rucksack)//2:len(rucksack)]
+        # Split rucksack into halves
+        part1 = rucksack[0:len(rucksack)//2]
+        part2 = rucksack[len(rucksack)//2:len(rucksack)]
         
-        print (rucksack)
-        print(half1, half2)
+        # Compare strings, find common items
+        items_in_both.extend(set([character for character in part1 if part2.__contains__(character)]))
+    
+    ## Part 1
+    # Sum of priorities of common items
+    priorityTotal = sum((priority.index(str(char)) + 1) for char in items_in_both)
+    print(priorityTotal)
+    
+        
+############################
+
+# Run script with chosen file
+
+if __name__ == "__main__":
+    # Change file
+    #######
+    day = "day03"
+    file = "input.txt"
+    #######
+
+    # Get absolute filepath of file
+    cwd = os.getcwd()
+    filepath = os.path.join(cwd, day, file)
+    
+    day03(filepath)
+
+    
+########### EOF ############

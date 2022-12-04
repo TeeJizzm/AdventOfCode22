d8de17dbd721ba0641df75065fd9f7dc5a0e131c
teej <tylerjay2010@live.co.uk>
2022-12-04 01:01:58 +0000

    Day03 - string split done

--- day03/day03.py
+++ day03/day03.py
@@ -1,9 +1,24 @@
 import os
 
-with open("example.txt") as file:
+#######
+day = "day03"
+file = "example.txt"
+#######
+
+# Get absolute filepath of file
+cwd = os.getcwd()
+filepath = os.path.join(cwd, day, file)
+
+with open(filepath) as file:
 
     rucksacks = file.read().splitlines()
 
     for rucksack in rucksacks:
+        
+        print(len(rucksack))
 
-        partone = rucksack.split(len(rucksack) / 2)
+        half1 = rucksack[0:len(rucksack)//2]
+        half2 = rucksack[len(rucksack)//2:len(rucksack)]
+        
+        print (rucksack)
+        print(half1, half2)

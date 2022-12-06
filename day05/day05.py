7f3e20c89ea1f8e41aa883d65becb52189b1182c
Teejizzm <teejizzm@gmail.com>
2022-12-06 16:43:48 +0000

    Day 05 - Part 1 Complete

--- day05/day05.py
+++ day05/day05.py
@@ -4,26 +4,66 @@ import tools.texttolists as tl
 
 ############################
 
+def cargoCrane9000(crates, mv, frm, to):
+    print("Move ", mv, " from ", frm, " to ", to)
+
+    for moves in range(mv):
+        crates[to-1].append(crates[frm-1].pop())
+
+def cargoCrane9001(crates, mv, frm, to):
+    print("Move ", mv, " from ", frm, " to ", to) 
+
+    temp = []
+    for moves in range(mv):
+        crates[to-1].append(crates[frm-1].pop())
+
+    #crates[to-1].append(temp.reverse())
+
+
+
 def day05(filepath):
     print("Day 5 - Supply Stacks")
 
-    crates, instructions = tl.toLists(filepath, "\n\n", "\n")
+    # Read file
+    crateIns, instructions = tl.toLists(filepath, "\n\n", "\n")
+    
+    ### Setup crates
+    crateNums = crateIns.pop()
+    crateIns.reverse()
+
+    crates = []
+
+    # Assign crates to variables
+    for i in range(1, len(crateNums), 4):
+        #print(crateNums[i])
+
+        crate = []
+        for j in range(len(crateIns)):
+            if crateIns[j][i] != " ":
+                crate.append(crateIns[j][i])
+    
+        crates.append(crate) # Crates list finished
+    
+    crates2 = crates
+    print(crates2)
     print(crates)
     
-    ### Decode Crates
-    
-    crateNums = crates.pop()
-    
-    for i in range(1, len(crateNums), 4):
-        print(crateNums[i])
-    
-    
-    
     ### Decode instructions
-    
-    
+    for instruction in instructions:
+        _,mv,_,frm,_,to = instruction.split(" ")
+        cargoCrane9000(crates, int(mv), int(frm), int(to)) # perform movements
+        cargoCrane9001(crates2, int(mv), int(frm), int(to))
 
+        print(crates)
+        print(crates2)
 
+    ### Results
+    part1, part2 = "", ""
+    for i in range(len(crates)):
+        part1 += str(crates[i][-1])
+        part2 += str(crates2[i][-1])
+    print("Part 1: ", part1)
+    print("Part 2: ", part2)
 
 ############################
 

1e8d96c193191e6657322b7dd870eaeedf240e6a
Teejizzm <teejizzm@gmail.com>
2022-12-12 15:40:38 +0000

    Day 08 - Part 2 working example

--- day08/day08.py
+++ day08/day08.py
@@ -9,29 +9,53 @@ def isVisible(trees, x, y):
 
     #print(" # ", trees[:x, y], trees[x, y], trees[x+1:, y])
 
-    if visibility(trees[x, y], trees[x, :y]):
+    if seesEdge(trees[x, y], trees[x, :y]) is True:
         return 1
-    if visibility(trees[x, y], trees[x, y+1:]):
+    if seesEdge(trees[x, y], trees[x, y+1:]) is True:
         return 1
-    if visibility(trees[x, y], trees[:x, y]):
+    if seesEdge(trees[x, y], trees[:x, y]) is True:
         return 1
-    if visibility(trees[x, y], trees[x+1:, y]):
+    if seesEdge(trees[x, y], trees[x+1:, y]) is True:
         return 1
     
     return 0
+
+def isScenic(trees, x, y):
+
+    print(" # ", trees[:x, y], trees[x, y], trees[x+1:, y])
+
+    up = seesTrees(trees[x, y], (trees[x, y-1::-1])) 
+    down = seesTrees(trees[x, y], trees[x, y+1:]) 
+    left = seesTrees(trees[x, y], (trees[x-1::-1, y])) 
+    right = seesTrees(trees[x, y], trees[x+1:, y])
+    
+    print(up, down, left, right)
+
+    score = up * down * left * right
+
+    return score
     
 
-def visibility(tree, row):
+def seesEdge(tree, row):
     #print(tree, ":", row)
     if tree > max(row):
         return True
     else:
         return False
 
+def seesTrees(tree, row):
+    # Return number of trees until view is blocked
+    if tree > max(row): # No trees block view, return number of trees
+        print("len", len(row), row)
+        return len(row)
+    else: # Return first index of blocking tree
+        print("fst", np.where(tree <= row)[0][0] + 1, row)
+        return (np.where(tree <= row)[0][0]+1)
+
 
 def day08(filepath):
     print("Day 08 - Treetop Tree House")
-    
+
     data = tl.toLists(filepath, "_", "\n")
 
     ## Data formatting
@@ -49,11 +73,16 @@ def day08(filepath):
     # Count for visible trees, begins with each tree in the perimeter.
     visibleTrees = (rows + cols - 2) * 2 
 
+    # Variable for part 2
+    scenicScore = []
+
     for x in range(1, rows - 1):
         for y in range(1, cols - 1):
             visibleTrees += isVisible(trees, x, y)
+            scenicScore.append(isScenic(trees, x, y))
 
     print("Part 1: ", visibleTrees)
+    print("Part 2: ", max(scenicScore))
 
 ############################
 
@@ -63,7 +92,7 @@ if __name__ == "__main__":
     # Change file
     #######
     day = "day08" 
-    file = "input.txt"
+    file = "example.txt"
     #######
     
     # Get absolute filepath of file

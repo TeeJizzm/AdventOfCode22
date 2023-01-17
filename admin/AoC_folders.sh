3d631c38f3cceb8ea6700a420e5f40c632043cc6
Teejizzm <teejizzm@gmail.com>
2023-01-17 13:31:43 +0000

    improved folder creation script

--- admin/AoC_folders.sh
+++ admin/AoC_folders.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+###################################################
+#
 
+###################################################
+# Generate folders                                #
+###################################################
 for N in {01..25}
 do
     #echo "Day $N"
     mkdir -p day$N
-    touch day$N/{day$N.md,ex.txt,in.txt}
-done
 
+    # Other files
+    touch day$N/{day$N.md,ex.txt,in.txt}
+
+    # Initiate Python file for each day from template
+    sed "s/XX/$N/g" python_template > day$N/day$N.py
+
+    # Initiate rust package - VScode will support this easily this way
+    cargo new day$N/rs --quiet --vcs none
+
+done
+###################################################
 echo "Created $N folders"

5ff6881c59349118a998f1691d9a3c9e7f8e2ad4
teej <tylerjay2010@live.co.uk>
2023-01-25 22:42:13 +0000

    AoC_folders script updated with tools folder

--- admin/AoC_folders.sh
+++ admin/AoC_folders.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
+
 ###################################################
 
-
+THIS_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
+echo From $THIS_PATH
 
 
 ###################################################
 # Generate folders                                #
 ###################################################
+# Folder for tools
+
+mkdir tools
+cp -r $THIS_PATH/python_tools/* tools
+
+
+# Folder for each day
 for N in {01..25}
 do
     #echo "Day $N"
-    mkdir -p day$N
+    mkdir -p day$N/{src,inc}
 
     # Other files
-    touch day$N/{day$N.md,ex.txt,in.txt}
+    touch day$N/day$N.md
+    touch day$N/inc/{ex.txt,in.txt}
 
     ## Python
     # Insert a Python file for each day from template
-    sed "s/XX/$N/g" python_template > day$N/day$N.py
+    sed "s/XX/$N/g" $THIS_PATH/python_template > day$N/day$N.py
 
     ## Rust
     # Initiate rust package - VScode will support this
-    cargo new day$N/rs --quiet --vcs none
-
-    ## Tools + scripts
-    # Copy tools folder
-    cp -R Tools 
+    cargo init day$N --quiet --vcs none
 
 done
 ###################################################

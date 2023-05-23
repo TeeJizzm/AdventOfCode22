43033ccfb41825d57aaab8ad105f7908c68bf871
teej <tylerjay2010@live.co.uk>
2023-05-23 20:03:28 +0100

    Day 02 - Complete in Go

--- day02/day02.go
+++ day02/day02.go
@@ -14,8 +14,9 @@ import (
 	// Input/Output
 	"fmt"
 	"io/ioutil"
+
 	// String manipulation
-	//"strings"
+	"strings"
 	//"strconv"
 	// Other required imports
 )
@@ -24,13 +25,13 @@ import (
 // Functions */
 
 func main() {
-	fmt.Println("Day 02 - *NAME*")
+	fmt.Println("Day 02 - Rock Paper Scissors")
 
 	// ------------ File -------------
 
-	//*/ - File switch
+	/*// - File switch
 	file, err := ioutil.ReadFile("day02/inc/ex.txt")
-	/*/ //
+	/*/
 	file, err := ioutil.ReadFile("day02/inc/in.txt")
 	// --- End --- */
 
@@ -38,15 +39,40 @@ func main() {
 		fmt.Println(err)
 	}
 
-	text := string(file)
-	//fmt.Print(text)
+	// Added fix for Windows newline
+	text := strings.ReplaceAll(string(file), "\r\n", "\n")
+	//fmt.Println(text)
 
 	// ----------- Setup -------------
 
-	part1 := ""
-	part2 := ""
+	rounds := strings.Split(text, "\n")
+
+	var part1 uint = 0
+	var part2 uint = 0
+
+	matrix := map[string]uint8{"A": 1, "B": 2, "C": 3,
+		"X": 1, "Y": 2, "Z": 3}
+
+	var part1_mat = map[uint8]map[uint8]uint8{
+		1: {1: 4, 2: 8, 3: 3},
+		2: {1: 1, 2: 5, 3: 9},
+		3: {1: 7, 2: 2, 3: 6}}
+
+	var part2_mat = map[uint8]map[uint8]uint8{
+		1: {1: 3, 2: 4, 3: 8},
+		2: {1: 1, 2: 5, 3: 9},
+		3: {1: 2, 2: 6, 3: 7}}
 
 	// ----------- Output ------------
+
+	for _, round := range rounds {
+		plays := strings.Split(round, " ")
+
+		part1 += uint(part1_mat[matrix[plays[0]]][matrix[plays[1]]])
+
+		part2 += uint(part2_mat[matrix[plays[0]]][matrix[plays[1]]])
+
+	}
 
 	fmt.Println("Part 1:", part1)
 	fmt.Println("Part 2:", part2)
